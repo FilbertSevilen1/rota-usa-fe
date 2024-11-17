47e22a9c2e800a754208b244fd52d8b742ab81d2
Ignatius Filbert Sevilen <81659972+FilbertSevilen1@users.noreply.github.com>
2024-11-17 23:23:23 +0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -659,7 +659,7 @@ function Dashboard() {
             </div>
 
             <div className="w-full md:w-[360px] mt-12">
-              <video width="100%" height="480px" controls>
+              <video width="100%" height="480px" controls className="rounded-xl">
                 <source src={video1} type="video/mp4"/>
                 Your browser does not support the video tag.
               </video>
