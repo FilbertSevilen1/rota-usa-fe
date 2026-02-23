fe4c79e4e35510a8d8e3d0fcbe5b9e2ec070e1d8
Ignatius Filbert Sevilen <81659972+FilbertSevilen1@users.noreply.github.com>
2026-02-23 16:08:48 +0700

    Add Nissan Car

--- src/components/WheelsEditor.jsx
+++ src/components/WheelsEditor.jsx
@@ -1,160 +1,4 @@
 import "../css/background.css";
-// Brand
-import brandBmw from "../assets/brands/bmw.webp";
-import brandFerrari from "../assets/brands/ferrari.webp";
-import brandHonda from "../assets/brands/honda.webp";
-import brandLamborghini from "../assets/brands/lamborghini.webp";
-import brandMercedesBenz from "../assets/brands/mercedes-benz.webp";
-import brandSubaru from "../assets/brands/subaru.webp";
-import brandToyota from "../assets/brands/toyota.webp";
-import brandMitsubishi from "../assets/brands/mitsubishi.webp";
-
-//Car
-import carBmw1Color1 from "../assets/cars/carbmw1.webp";
-
-import carLamborghini1Color1 from "../assets/cars/carangled1.webp";
-import carLamborghini1Color2 from "../assets/cars/carangled2.webp";
-import carLamborghini1Color3 from "../assets/cars/carangled3.webp";
-import carLamborghini1Color4 from "../assets/cars/carangled4.webp";
-import carLamborghini1Color5 from "../assets/cars/carangled5.webp";
-
-import carHonda1Color1 from "../assets/cars/carhonda1.webp";
-import carHonda1Color2 from "../assets/cars/carhonda2.webp";
-import carHonda1Color3 from "../assets/cars/carhonda3.webp";
-import carHonda1Color4 from "../assets/cars/carhonda4.webp";
-import carHonda1Color5 from "../assets/cars/carhonda5.webp";
-import carHonda1Color6 from "../assets/cars/carhonda6.webp";
-import carHonda1Color7 from "../assets/cars/carhonda7.webp";
-import carHonda1Color8 from "../assets/cars/carhonda8.webp";
-import carHonda1Color9 from "../assets/cars/carhonda9.webp";
-
-import carFerrari1Color1 from "../assets/cars/carangled6.webp";
-import carFerrari1Color2 from "../assets/cars/carangled7.webp";
-import carFerrari1Color3 from "../assets/cars/carangled8.webp";
-import carFerrari1Color4 from "../assets/cars/carangled9.webp";
-import carFerrari1Color5 from "../assets/cars/carangled10.webp";
-import carFerrari1Color6 from "../assets/cars/carangled11.webp";
-
-import carSubaru1Color1 from "../assets/cars/carsubaru1.webp";
-import carSubaru1Color2 from "../assets/cars/carsubaru2.webp";
-import carSubaru1Color3 from "../assets/cars/carsubaru3.webp";
-import carSubaru1Color4 from "../assets/cars/carsubaru4.webp";
-import carSubaru1Color5 from "../assets/cars/carsubaru5.webp";
-
-import carToyota1Color1 from "../assets/cars/cartoyotatundra1.webp";
-import carToyota1Color2 from "../assets/cars/cartoyotatundra2.webp";
-import carToyota1Color3 from "../assets/cars/cartoyotatundra3.webp";
-import carToyota1Color4 from "../assets/cars/cartoyotatundra4.webp";
-import carToyota1Color5 from "../assets/cars/cartoyotatundra5.webp";
-import carToyota1Color6 from "../assets/cars/cartoyotatundra6.webp";
-import carToyota1Color7 from "../assets/cars/cartoyotatundra7.webp";
-import carToyota1Color8 from "../assets/cars/cartoyotatundra8.webp";
-import carToyota1Color9 from "../assets/cars/cartoyotatundra9.webp";
-
-import carMitsubishi1Color1 from "../assets/cars/carmitsubishi1.webp";
-import carMitsubishi1Color2 from "../assets/cars/carmitsubishi2.webp";
-import carMitsubishi1Color3 from "../assets/cars/carmitsubishi3.webp";
-import carMitsubishi1Color4 from "../assets/cars/carmitsubishi4.webp";
-import carMitsubishi1Color5 from "../assets/cars/carmitsubishi5.webp";
-import carMitsubishi1Color6 from "../assets/cars/carmitsubishi6.webp";
-import carMitsubishi1Color7 from "../assets/cars/carmitsubishi7.webp";
-import carMitsubishi1Color8 from "../assets/cars/carmitsubishi8.webp";
-import carMitsubishi1Color9 from "../assets/cars/carmitsubishi9.webp";
-
-//Wheel
-import wheel1 from "../assets/wheels/2024 02 16 - photo rota/ATLAS 1/ATLAS 1 17_17x8.5_4_5x139.7_106.1_ RL YMH BLK/plain/DSCF7736 1nobg.webp";
-import wheel2 from "../assets/wheels/2024 02 16 - photo rota/CIRCUIT 8/CIRCUIT 8_15x6.5_38_8x100x100_67.1_BRONZE/plain/DSCF7684 nobg.webp";
-import wheel3 from "../assets/wheels/2024 02 16 - photo rota/CIRCUIT 10/CIRCUIT 10_15X6.5_45_4X100_67.1_RF SP BRONZE/plain/DSCF7400 3nobg.webp";
-import wheel4 from "../assets/wheels/2024 02 16 - photo rota/D2/D2_16x7_40_5x114_73_RL CARBON FIBER/plain/DSCF7895 1nobg.webp";
-import wheel5 from "../assets/wheels/2024 02 16 - photo rota/DPT/DPT_17x8_48_10x114x100_73_HYPER BLACK/plain/DSCF7640nobg.webp";
-import wheel6 from "../assets/wheels/2024 02 16 - photo rota/FIGHTER/FIGHTER 10H 1670_16X7_40_10X114.3X100_73_HYPER SILVER IMP1/plain/DSCF8177nobg.webp";
-import wheel7 from "../assets/wheels/2024 02 16 - photo rota/FIVE STAR OFF/FIVE STAR OFF_16x8_10_6x139.7_110_BRONZE/plain/DSCF7417nobg.webp";
-import wheel8 from "../assets/wheels/2024 02 16 - photo rota/G-FORCE/G-FORCE_17x9_42_5x100_73_HYPER BLACK/plain/DSCF7469nobg.webp";
-import wheel9 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x7_0_4x100_67.1_RF SP BRONZE/plain/DSCF7533 6nobg.webp";
-import wheel10 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x7_20_4x100_67.1_BRONZE/plain/DSCF7945nobg.webp";
-import wheel11 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x7_20_4x100_67.1_WHITE/plain/DSCF7835 1nobg.webp";
-import wheel12 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x8_0_4x100_67.1_RF P SILVER/plain/DSCF7748 1nobg.webp";
-import wheel13 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x8_0_5x114_73_FLAT GUN YMH BLK LIP/plain/DSCF7813 1nobg.webp";
-import wheel14 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_15x9_-15_4x114_73_RF SP BRONZE/plain/DSCF7799 3nobg.webp";
-import wheel15 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_16x8_20_4x100_67.1_SATIN BLACK/plain/DSCF7956nobg.webp";
-import wheel16 from "../assets/wheels/2024 02 16 - photo rota/GRID CLASSIC/GRID CLASSIC_16x8_20_5x114_73_WHITE/plain/DSCF7779 1nobg.webp";
-
-// Concave
-import wheel17 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15x8_20_5x114_73_HYPER BLACK/plain/DSCF7793 1nobg.webp";
-import wheel18 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15x8_20_5x114_73_WHITE/plain/DSCF7938nobg.webp";
-import wheel19 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15X8_20_5X114.3_73_PLAIN SILVER/plain/DSCF8518nobg.webp";
-import wheel20 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15X8_20_5X114.3_73_RF P SILVER/plain/DSCF8512nobg.webp";
-import wheel21 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15X8_20_5X114.3_73_RF SP BRONZE/plain/DSCF8475nobg.webp";
-import wheel22 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15x9_36_4x100_67.1_HYPER BLACK/plain/DSCF7588nobg.webp";
-import wheel23 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_15x9_36_4x100_67.1_SPEED BRONZE/plain/DSCF7502nobg.webp";
-import wheel24 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_16X8_20_5X114.3_73_RF P SILVER/plain/DSCF8497nobg.webp";
-import wheel25 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_16X8_20_5X114.3_73_SATIN BLACK/plain/DSCF8489nobg.webp";
-import wheel26 from "../assets/wheels/2024 02 16 - photo rota/GRID CONCAVE/GRID CONCAVE_16X8_20_5X114.3_73_WHITE/plain/DSCF8500nobg.webp";
-
-// Racing
-import wheel27 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_15x6.5_38_4x100_67.1_SPORT BRONZE/plain/DSCF7949nobg.webp";
-import wheel28 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_15x7_38_4x100_67.1_PCWH_WHITE/plain/DSCF7785 1nobg.webp";
-import wheel29 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_16x7_42_4x108_63.35_SATIN BLACK/plain/DSCF7820 1nobg.webp";
-import wheel30 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_17x7.5_45_5x114_73_PCSB_RF SP BRONZE/plain/DSCF7594nobg.webp";
-import wheel31 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_17x8_40_4x108_73_HYPER BLACK/plain/DSCF7656nobg.webp";
-import wheel32 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_17x8_40_4x108_73_WHITE/plain/DSCF7695 1nobg.webp";
-import wheel33 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_17x8_44_5x100_73_PLAIN GOLD/plain/DSCF7829 1nobg.webp";
-import wheel34 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_17x9_42_5x114_73_SATIN BLACK/plain/DSCF7907nobg.webp";
-import wheel35 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_18x9.5_20_5x114_73_PLAIN GOLD/plain/DSCF7914nobg.webp";
-import wheel36 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_18x10_30_5x100_73_FLAT BLACK/plain/DSCF7510nobg.webp";
-import wheel37 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19x8.5_44_5x114_73_CHAMELEON/plain/DSCF7478nobg.webp";
-import wheel38 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19x8.5_44_5x114_73_SPEED BRONZE/plain/DSCF7520nobg.webp";
-import wheel39 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19x9.5_38_5x114_73_TITANIUM CHROME/plain/DSCF7485nobg.webp";
-import wheel40 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19X9.5_45_5X120_64.1_COSMIC BLUE/plain/DSCF8436nobg.webp";
-import wheel41 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19X9.5_45_5X120_64.1_RF SP BRONZE/plain/DSCF8445nobg.webp";
-import wheel42 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19x9.5_45_5X120_64.1_SATIN BLACK/plain/DSCF8452nobg.webp";
-import wheel43 from "../assets/wheels/2024 02 16 - photo rota/GRID RACING/GRID RACING_19x10.5_15_5x114_73_FLAT BLACK/plain/DSCF7502nobg.webp";
-
-import wheel44 from "../assets/wheels/2024 02 16 - photo rota/HB10/HB10_15x7_35_4x100_56.1_RL GUNMETALLIC/plain/DSCF7436nobg.webp";
-import wheel45 from "../assets/wheels/2024 02 16 - photo rota/HUNTER/HUNTER_15x7_35_4x100_67.1_SPORT BRONZE/plain/DSCF7527nobg.webp";
-import wheel46 from "../assets/wheels/2024 02 16 - photo rota/J-SPL/J-SPL_15x6.5_37_4x100_67.1_RL CARBON FIBER/plain/DSCF7902nobg.webp";
-
-import wheel47 from "../assets/wheels/2024 02 16 - photo rota/KB-R/KBR_18X9.5_38_5X100_73_WHITE/plain/DSCF8139nobg.webp";
-import wheel48 from "../assets/wheels/2024 02 16 - photo rota/KB-R/KBR_18x9.5_38_5x114_73_SATIN BLACK/plain/DSCF7668nobg.webp";
-import wheel49 from "../assets/wheels/2024 02 16 - photo rota/KB-R/KBR_18X9.5_38_5X114.3_73_PLAIN GOLD/plain/DSCF8153nobg.webp";
-
-import wheel50 from "../assets/wheels/2024 02 16 - photo rota/KENSEI/KENSEI_15x9_-15_4x114_73_RL HYP BLK/plain/DSCF7610nobg.webp";
-
-import wheel51 from "../assets/wheels/2024 02 16 - photo rota/MXR/MXR-R2 18_18x11_20_5x114_73_RL HYP BLK/plain/DSCF7495nobg.webp";
-
-import wheel52 from "../assets/wheels/2024 02 16 - photo rota/OS MESH/OS MESH 15_15x8_20_4x100_67.1_RL STEEL GRAY/plain/DSCF7625nobg.webp";
-
-import wheel53 from "../assets/wheels/2024 02 16 - photo rota/PSD/PSD 17_17x7.5_35_5x130_71.6_FSL FSB FBI/plain/DSCF7881 1nobg.webp";
-import wheel54 from "../assets/wheels/2024 02 16 - photo rota/PSD/PSD 17_17x7.5_35_5x130_71.6_RL YMH BLACK/plain/DSCF7721nobg.webp";
-import wheel55 from "../assets/wheels/2024 02 16 - photo rota/PSD/PSD 17_17x9_16_5x130_71.6_FSL FSB FBI/plain/DSCF7715 1nobg.webp";
-import wheel56 from "../assets/wheels/2024 02 16 - photo rota/PSD/PSD 17_17x11_16_5x130_71.6_FSL FSP FBI/plain/DSCF7706nobg.webp";
-import wheel57 from "../assets/wheels/2024 02 16 - photo rota/PSD/PSD 17_17x11_16_5x130_71.6_RL YMH BLK/plain/DSCF7700nobg.webp";
-
-import wheel58 from "../assets/wheels/2024 02 16 - photo rota/R-SPEC/R-SPEC 16_16x7_45_4x100_67.1_SATIN BLACK/plain/DSCF7762 1nobg.webp";
-
-import wheel59 from "../assets/wheels/2024 02 16 - photo rota/RB/RB 15_15x7_4_4x114_73_RL P SILVER/plain/DSCF7560nobg.webp";
-import wheel60 from "../assets/wheels/2024 02 16 - photo rota/RB/RB 15_15X7_4_4X114.3_73_SATIN BLACK/plain/DSCF8483nobg.webp";
-import wheel61 from "../assets/wheels/2024 02 16 - photo rota/RB/RB 15_15x7_12_4x114_73_RL P SILVER/plain/DSCF7552nobg.webp";
-import wheel62 from "../assets/wheels/2024 02 16 - photo rota/RB/RB 16_16X7_40_4X100_56.1_RL GUNMETALLIC/plain/DSCF7757 2nobg.webp";
-import wheel63 from "../assets/wheels/2024 02 16 - photo rota/RB/RB 17_17X7.5_45_4X100_56.1_RL P SILVER/plain/DSCF7926 1nobg.webp";
-import wheel64 from "../assets/wheels/2024 02 16 - photo rota/RB/RB REAR 17_17X8.5_4_4X114.3_73_RL HYP BLACK/plain/DSCF7645nobg.webp";
-import wheel65 from "../assets/wheels/2024 02 16 - photo rota/RB/RB_13x8_4_4x114_73_RL P SILVER/plain/DSCF7690nobg.webp";
-import wheel66 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9_-13_4X114.3_73_RL GUNMETALLIC/plain/DSCF7855 2nobg.webp";
-import wheel67 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9_-13_4X114.3_73_RL HYP BLACK/plain/DSCF7867 1.webp";
-import wheel68 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9_-13_4X114.3_73_RL YMH BLK/plain/DSCF7861 2nobg.webp";
-import wheel69 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9.5_-19_4X114.3_73_RL GUNMETALLIC/plain/DSCF7841 2nobg.webp";
-import wheel70 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9.5_-19_4X114.3_73_RL HYP BLACK/plain/DSCF7848 2nobg.webp";
-import wheel71 from "../assets/wheels/2024 02 16 - photo rota/RB/RB-X 17_17X9.5_-19_4X114.3_73_RL YMH BLK/plain/DSCF7874 1nobg.webp";
-
-import wheel72 from "../assets/wheels/2024 02 16 - photo rota/RECCE/RECCE_17X7.5_40_4X108_63.35_HYPER BLACK/plain/DSCF7429nobg.webp";
-import wheel73 from "../assets/wheels/2024 02 16 - photo rota/SHAKOTAN/SHAKOTAN_15X8_0_4X100_67.1_RF YMH BLACK/plain/DSCF7633nobg.webp";
-
-import wheel74 from "../assets/wheels/2024 02 16 - photo rota/SLIP STREAM/SLIP STREAM_15X6.5_40_4X100_67.1_RF GUNMETALLIC/plain/DSCF7580nobg.webp";
-import wheel75 from "../assets/wheels/2024 02 16 - photo rota/SLIP STREAM/SLIP STREAM_15X7_35_5X100_57.1_PLAIN SILVER/plain/DSCF7741 1nobg.webp";
-import wheel76 from "../assets/wheels/2024 02 16 - photo rota/SLIP STREAM/SLIP STREAM_15X7_40_4X100_56.1_RF GUNMETALLIC/plain/DSCF7544nobg.webp";
-import wheel77 from "../assets/wheels/2024 02 16 - photo rota/SLIP STREAM/SLIP STREAM_15X7_40_5X114.3_73_SATIN BLACK/plain/DSCF7567nobg.webp";
-import wheel78 from "../assets/wheels/2024 02 16 - photo rota/SLIP STREAM/SLIPSTREAM R_18X9.5_40_5X120_64.1_MAG BLK/plain/DSCF8591nobg.webp";
-
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
@@ -582,6 +426,13 @@ function WheelsEditor() {
           "mt-[-155px] xl:mt-[-310px] w-[2.1rem] h-[3.6rem] ml-[70px] xl:w-[4.2rem] xl:h-[7.2rem] xl:ml-[140px] rounded-full relative wheels bg-gray-950",
         wheelTemplate2:
           "mt-[-146px] xl:mt-[-292px] w-[5.7rem] h-[6.3rem] mr-[172.5px] xl:w-[11.4rem] xl:h-[12.6rem] xl:mr-[345px] rounded-full relative wheels bg-gray-950",
+      },
+      {
+        car_id: 18,
+        wheelTemplate1:
+          "mt-[-125px] xl:mt-[-250px] w-[3.9rem] h-[4.2rem] ml-[218px] xl:w-[7.8rem] xl:h-[8.4rem] xl:ml-[436px] rounded-full relative wheels bg-gray-950",
+        wheelTemplate2:
+          "mt-[-139px] xl:mt-[-278px] w-[2.0rem] h-[3.5rem] mr-[60px] xl:w-[4.0rem] xl:h-[7rem] xl:mr-[120px] rounded-full relative wheels bg-gray-950",
       },
     ];
 
